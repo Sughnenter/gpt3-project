bcb548b236006dbcd50d10d00e71c73f5bfbffad
shammah <shamaj2006@gmail.com>
2025-12-24 11:44:56 +0100

    created article component and added styles

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -5,7 +5,14 @@ function Article ({imageUrl, date, title}) {
   return (
     <div className='gpt3__blog-container_article'>
       <div className='gpt3__blog-container_article-image'>
-        <img src={imageUrl} alt="blog image" />
+        <img src={imageUrl} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
       </div>
     </div>
   )
